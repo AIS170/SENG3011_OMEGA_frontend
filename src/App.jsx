1b30dfa1d36a8713fe0c0a875a9ca59d29045c23
AIS170 <anujindersingh@gmail.com>
2025-04-12 03:51:09 +1000

    made a homepage

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import Login from "./login"
 import Signup from "./Signup"
 import ConfirmSignup from "./Confirm"
 import { BrowserRouter as Router, Routes, Route } from "react-router"
+import Home from "./Home"
 
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Signup/>} />
+        <Route path="/" element={<Home/>} />
+        <Route path="/signup" element={<Signup/>} />
         <Route path="/confirm-signup" element={<ConfirmSignup/>} />
         <Route path="/login" element={<Login/>} />
       </Routes>
