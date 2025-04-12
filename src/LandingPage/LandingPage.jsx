16906acaeb40cfcb135bcf8a41405644e61729da
BobHell0 <anujan.cenan@gmail.com>
2025-04-13 09:58:19 +1000

    started a landing page

---
+++ src/LandingPage/LandingPage.jsx
@@ -0,0 +1,35 @@
+import "./LandingPage.css"
+export default function LandingPage() {
+  return (
+    <div>
+      <section id="LandingPageSection1">
+        <div id="LandingPage_IntroductoryText">
+          <h1 id="LandingPage_OmegaInvesting">OMEGA Investing</h1>
+          <h4 id="LandingPage_StockMarketSimplified">The stock market simplified</h4>
+        </div>
+        <button id="GetStartedButton">Get Started</button>
+
+      </section>
+
+      <section id="LandingPageSection2">
+        <div id="LandingPageSection2_bgImage"></div>
+
+        <div id="LandingPage_Analysis">
+          <h2 id="LandingPage_CuttingEdgeAnalysis">Invest with Confidence</h2>
+          <h5 id="LandingPage_AnalysisExplained">OMEGA uses real time financial and news data to make predictions on the future performance of different stocks in the stock market</h5>
+        </div>
+      </section>
+
+      <section id="LandingPageSection3">
+        <div id="LandingPageSection3_bgImage"></div>
+
+        <div id="LandingPage_Free">
+          <h2 id="LandingPage_CuttingEdgeAnalysis">Invest with Confidence</h2>
+          <h5 id="LandingPage_AnalysisExplained">OMEGA uses real time financial and news data to make predictions on the future performance of different stocks in the stock market</h5>
+        </div>
+      </section>
+
+
+    </div>
+  )
+}
