3414314487bb7ca5a91b83a3acefd3af4d490632
BobHell0 <anujan.cenan@gmail.com>
2025-04-13 12:20:40 +1000

    landing page; 4 sections with wallpaper images and boilerplat text

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import Login from "./login"
+import LandingPage from "./LandingPage/LandingPage"
 import Signup from "./Signup"
 import ConfirmSignup from "./Confirm"
 import { BrowserRouter as Router, Routes, Route } from "react-router"
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/" element={<Signup/>} />
         <Route path="/confirm-signup" element={<ConfirmSignup/>} />
         <Route path="/login" element={<Login/>} />
+        <Route path="/landing" element={<LandingPage />} />
       </Routes>
     </Router>
   )
--- src/LandingPage/LandingPage.jsx
+++ src/LandingPage/LandingPage.jsx
@@ -1,8 +1,12 @@
 import "./LandingPage.css"
 export default function LandingPage() {
+
+  function handleScroll() {
+    console.log("Hello");
+  }
   return (
-    <div>
-      <section id="LandingPageSection1">
+    <div id="LandingPage_Container" >
+      <section id="LandingPageSection1" onScroll={() => handleScroll()}>
         <div id="LandingPage_IntroductoryText">
           <h1 id="LandingPage_OmegaInvesting">OMEGA Investing</h1>
           <h4 id="LandingPage_StockMarketSimplified">The stock market simplified</h4>
@@ -11,21 +15,25 @@ export default function LandingPage() {
 
       </section>
 
-      <section id="LandingPageSection2">
-        <div id="LandingPageSection2_bgImage"></div>
 
-        <div id="LandingPage_Analysis">
-          <h2 id="LandingPage_CuttingEdgeAnalysis">Invest with Confidence</h2>
-          <h5 id="LandingPage_AnalysisExplained">OMEGA uses real time financial and news data to make predictions on the future performance of different stocks in the stock market</h5>
+      <section id="LandingPageSection2">
+        <div id="LandingPage_Analysis"  className="LandingPage-leftText">
+          <h2>Invest with Confidence</h2>
+          <h5>OMEGA uses real time financial and news data to make predictions on the future performance of different stocks in the stock market</h5>
         </div>
       </section>
 
       <section id="LandingPageSection3">
-        <div id="LandingPageSection3_bgImage"></div>
+        <div id="LandingPage_NewHeights" className="LandingPage-rightText">
+          <h2>Conquer new Heights</h2>
+          <h5>Take advantage of OMEGA's assistance to simplify complicated stock information into clearer conclusions</h5>
+        </div>
+      </section>
 
-        <div id="LandingPage_Free">
-          <h2 id="LandingPage_CuttingEdgeAnalysis">Invest with Confidence</h2>
-          <h5 id="LandingPage_AnalysisExplained">OMEGA uses real time financial and news data to make predictions on the future performance of different stocks in the stock market</h5>
+      <section id="LandingPageSection4">
+        <div id="LandingPage_GrowMoney" className="LandingPage-leftText">
+          <h2>Grow and Diversify</h2>
+          <h5>OMEGA's vast data collection means you have more choice in deciding what stocks will benefit you and your money.</h5>
         </div>
       </section>
 
