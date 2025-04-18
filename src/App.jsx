2d4d8d50bdd1990b26b227cd61d70d048a86f8e9
BobHell0 <anujan.cenan@gmail.com>
2025-04-18 19:06:13 +1000

    Added a transition (fade in) section for the landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,49 @@
-import React from "react"
-import Login from "./login"
-import LandingPage from "./LandingPage/LandingPage"
-import Signup from "./Signup"
-import ConfirmSignup from "./Confirm"
-import { BrowserRouter as Router, Routes, Route } from "react-router"
-
+import React from "react";
+import Login from "./login";
+import Signup from "./Signup";
+import ConfirmSignup from "./Confirm";
+import Dashboard from "./Dashboard/Dashboard";
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+import Home from "./Home";
+import Unauthorized from "./Unauthorised";
+import ProtectedRoute from "./Protected";
+import Analysis from "./Analysis";
+import Queries from "./Queries";
+import LandingPage from "./LandingPage/LandingPage";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Signup/>} />
-        <Route path="/confirm-signup" element={<ConfirmSignup/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/landing" element={<LandingPage />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/confirm-signup" element={<ConfirmSignup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/401" element={<Unauthorized />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/analysis"
+          element={
+            <ProtectedRoute>
+              <Analysis />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/ai-help"
+          element={
+            <ProtectedRoute>
+              <Queries />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   );
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,21 +1,49 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
+import "./LandingPage/LandingPage.css"
+import { useIsVisible } from "./IsVisible";
+import Navbar from "./Navbar/Navbar";
 
+// Change Log
+// added gap-[20px] to first div element - put some gap between the OMEGA gif and the Get Started Button
+// Changed colour of shadow when the Get Started bttn is hovered over
+
+
+// Found a "UseIsVisible (IsVisible.jsx) Hook which helps for 'fade in' transitions
+//      Section element has a screen-sized image
 export default function Home() {
   const navigate = useNavigate();
+
+  const section2Ref = useRef();
+  const isVisibleSec2 = useIsVisible(section2Ref)
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-black">
-      <img
-        src="/omega.gif"
-        alt="Omega Logo Animation"
-        className="w-2/3 max-w-md"
-      />
-      <button
-        onClick={() => navigate("/signup")}
-        className="mt-8 px-6 py-2 bg-gradient-to-r from-orange-400 via-pink-500 to-purple-600 text-white text-lg rounded shadow-lg hover:shadow-xl transition"
-      >
-        Get Started
-      </button>
-    </div>
-  );
+    <>
+    <div style={{ overflowY: 'scroll', height: '100vh' }}>
+    <Navbar currPage={"Home"}/>
+    
+      <div className="flex min-h-screen items-center flex-wrap justify-center h-max bg-black gap-[20px]">
+        <img
+          src="/omega.gif"
+          alt="Omega Logo Animation"
+          className="w-2/3 max-w-md"
+        />
+          <button
+            onClick={() => navigate("/signup")}
+            className="mt-8 px-6 py-2 bg-gradient-to-r from-orange-400 via-pink-500 to-purple-600 text-white text-lg rounded shadow-lg hover:shadow-md hover:shadow-cyan-100 transition hover:cursor-pointer"
+          >
+            Get Started
+          </button>
+      </div>
+
+      <section id="LandingPageSection2" ref={section2Ref} className={`transition-opacity ease-in duration-700 ${isVisibleSec2 ? "opacity-100" : "opacity-0"}`}>
+        <div id="LandingPage_Analysis"  className="LandingPage-leftText">
+          <h2>Invest with Confidence</h2>
+          <h5>OMEGA uses real time financial and news data to make predictions on the future performance of different stocks in the stock market</h5>
+        </div>
+      </section>
+    
+    </div>  
+    </>
+    );
 }
