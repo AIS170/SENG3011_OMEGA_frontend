b37dd7cf21f3a5f9a63126e63ca5ff8c258502cd
BobHell0 <anujan.cenan@gmail.com>
2025-04-18 19:05:34 +1000

    hook for determining whether an element has is visible in the viewport

---
+++ src/IsVisible.jsx
@@ -0,0 +1,20 @@
+// Taken from https://medium.com/@jacobvejlinjensen/how-to-create-a-smooth-appear-on-scroll-transition-with-tailwind-css-and-react-82f2a32ab295
+
+import { useEffect, useState } from "react";
+
+export function useIsVisible(ref) {
+  const [isIntersecting, setIntersecting] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      setIntersecting(entry.isIntersecting)
+    });
+    
+    observer.observe(ref.current);
+    return () => {
+      observer.disconnect();
+    };
+  }, [ref]);
+
+  return isIntersecting;
+}
