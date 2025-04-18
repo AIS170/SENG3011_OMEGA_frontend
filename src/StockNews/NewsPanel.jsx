89dcd5d3be8b2ff84ec65bc30a6a0769b14dc04d
BobHell0 <anujan.cenan@gmail.com>
2025-04-18 16:09:29 +1000

    created a news panel for the news feed page

---
+++ src/StockNews/NewsPanel.jsx
@@ -0,0 +1,32 @@
+export default function NewsPanel({
+  title,
+  description,
+  keywords,
+  url,
+  imageUrl,
+  publishedAt,
+  source
+}) {
+  const addDefaultImg = ev => {
+    ev.target.src = "./omega.png"
+  }
+
+  return (
+    <div className="flex relative w-[60%] mx-[12%] mb-[24px] mt-[24px] p-[2%] border-2 border-gray-400">
+      <div className="mt-[1%] flex justify-center items-center w-[40%]">
+        <img className="max-h-[100%]" src={imageUrl} alt={imageUrl} onError={addDefaultImg}/>
+      </div>
+      <div className="mt-[1%] w-[60%]">
+        <a href={url} className="underline"><h2 className="text-[26px] text-center px-[14px]">{title}</h2></a>
+        <br></br>
+        <p><b>Snippet:</b></p>
+        <h4>{description}</h4>
+        <br></br>
+
+        <div><b>Publication Date:</b> {publishedAt.split('T')[0]}</div>
+        <div><b>Source:</b> {source}</div>
+      </div>
+    </div>
+  )
+
+}
